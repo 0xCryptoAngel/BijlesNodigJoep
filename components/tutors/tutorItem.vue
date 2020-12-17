3dd5d7a9c7eaaaa2442c5ed5700d729f89bd839d
bryanballidev <bryanballidev@gmail.com>
2020-12-17 16:11:16 +0100

    design improvements

--- components/tutors/tutorItem.vue
+++ components/tutors/tutorItem.vue
@@ -20,19 +20,40 @@
               {{ name }}
             </h4>
           </NuxtLink>
-          <div class="mt-2">
-            {{ rate }}€
 
-            <span class="text-sm text-gray-600">per uur</span>
+          <div class="flex items-end flex-grow mt-2">
+            <div class="mr-4">
+              <span class="flex items-center">
+                <svg
+                  class="w-4 h-4 text-yellow-400"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1"
+                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                  ></path>
+                </svg>
+
+                <span class="text-sm font-semibold text-light-blue-800">{{
+                  rating
+                }}</span>
+                <span class="text-sm text-gray-600">
+                  ({{ totalReviews }})
+                </span>
+              </span>
+            </div>
+            <div class="ml-auto">
+              {{ rate }}€
+
+              <span class="text-sm text-gray-600">per uur</span>
+            </div>
           </div>
-          <div class="mt-2">
-            <span class="font-semibold text-light-blue-800"
-              >{{ rating }}/5 sterren</span
-            >
-            <span class="text-sm text-gray-600 truncate">
-              (na {{ totalReviews }} reviews)
-            </span>
-          </div>
+
           <div class="mt-2">{{ description }}</div>
           <div>
             <div class="mt-4 text-sm font-semibold text-gray-600">
