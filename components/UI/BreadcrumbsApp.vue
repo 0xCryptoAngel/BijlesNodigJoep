83e263077ddc09fcb4aef1727a5d245d44864cf8
bryanballidev <bryanballidev@gmail.com>
2021-01-01 16:33:21 +0100

    modified breadcrumbs

--- components/UI/BreadcrumbsApp.vue
+++ components/UI/BreadcrumbsApp.vue
@@ -1,10 +1,18 @@
 <template>
   <nav class="flex bg-white border-b border-gray-200" aria-label="Breadcrumb">
-    <ol class="flex w-full px-4 mx-12 space-x-4 sm:px-6 lg:px-2">
-      <li class="flex">
+    <ol
+      vocab="http://schema.org/"
+      typeof="BreadcrumbList"
+      class="flex w-full px-4 mx-12 space-x-4 sm:px-6 lg:px-2"
+    >
+      <li property="itemListElement" typeof="ListItem" class="flex">
         <div class="flex items-center">
-          <nuxt-link to="/dashboard" class="text-gray-400 hover:text-gray-500">
-            <!-- Heroicon name: home -->
+          <NLink
+            property="item"
+            typeof="WebPage"
+            to="/dashboard"
+            class="ml-4 text-sm text-gray-500 hover:text-gray-700"
+          >
             <svg
               class="flex-shrink-0 w-5 h-5"
               xmlns="http://www.w3.org/2000/svg"
@@ -16,15 +24,17 @@
                 d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
               />
             </svg>
-            <span class="sr-only">Home</span>
-          </nuxt-link>
+            <span class="sr-only" property="name">BijlesNodig</span>
+          </NLink>
+          <meta property="position" content="1" />
         </div>
       </li>
       <li
-        v-for="(item, i) in crumbs"
-        :key="i"
+        v-for="(crumb, index) in crumbs"
+        :key="index"
+        property="itemListElement"
+        typeof="ListItem"
         class="flex"
-        :class="item.classes"
       >
         <div class="flex items-center">
           <svg
@@ -37,12 +47,19 @@
           >
             <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
           </svg>
-          <nuxt-link
-            :to="item.path"
+          <NLink
+            property="item"
+            typeof="WebPage"
+            :to="crumb.path"
             class="ml-4 text-sm text-gray-500 hover:text-gray-700"
           >
-            {{ item.name }}
-          </nuxt-link>
+            <span property="name">{{
+              $route.fullPath === crumb.path && title !== null
+                ? title
+                : crumb.title
+            }}</span>
+          </NLink>
+          <meta property="position" :content="index + 2" />
         </div>
       </li>
     </ol>
@@ -50,32 +67,35 @@
 </template>
 
 <script>
+const titleCase = require('ap-style-title-case')
+
 export default {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+  },
+
   computed: {
     crumbs() {
+      const fullPath = this.$route.fullPath
+      const params = fullPath.startsWith('/')
+        ? fullPath.substring(1).split('/')
+        : fullPath.split('/')
       const crumbs = []
-      this.$route.matched.map((item, i, { length }) => {
-        const crumb = {}
-        crumb.path = item.path
-        crumb.name = this.$i18n.t(item.name || item.path)
 
-        // is last item?
-        if (i === length - 1) {
-          // is param route? .../.../:id
-          if (item.regex.keys.length > 0) {
-            crumbs.push({
-              path: item.path.replace(/\/:[^/:]*$/, ''),
-              name: this.$i18n.t(item.name.replace(/-[^-]*$/, '')),
-            })
-            crumb.path = this.$route.path
-            crumb.name = this.$i18n.t(this.$route.name, [
-              crumb.path.match(/[^/]*$/)[0],
-            ])
-          }
-          crumb.classes = 'font-semibold'
+      let path = ''
+
+      params.forEach((param, index) => {
+        path = `${path}/${param}`
+        const match = this.$router.match(path)
+        if (match.name !== null) {
+          crumbs.push({
+            title: titleCase(param.replace(/-/g, ' ')),
+            ...match,
+          })
         }
-
-        crumbs.push(crumb)
       })
 
       return crumbs
