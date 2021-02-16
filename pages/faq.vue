17424338f32f510b46183470ecc4b4888212d9d1
bryanballidev <bryanballidev@gmail.com>
2021-02-16 13:50:19 +0100

    website changes

---
+++ pages/faq.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <main>
+      <web-header :title="title" :beschrijving="beschrijving"></web-header>
+    </main>
+  </div>
+</template>
+
+<script>
+import webHeader from '~/components/webLayout/webHeader.vue'
+export default {
+  components: { webHeader },
+  data() {
+    return {
+      title: 'Veelgestelde vragen',
+      beschrijving:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.Uitleg over hoe alles werkt voor de student en bijlesgever',
+    }
+  },
+  layout: 'webpage',
+  middleware: 'auth',
+  auth: 'guest',
+}
+</script>
+
+<style></style>
