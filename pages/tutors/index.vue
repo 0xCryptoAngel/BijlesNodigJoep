a88f77fd5a5399ea291d756475ba4d53fcbe12cb
bryanballidev <bryanballidev@gmail.com>
2021-08-24 12:57:15 +0200

    markers on front

--- pages/tutors/index.vue
+++ pages/tutors/index.vue
@@ -342,9 +342,7 @@
                         ref="map-root"
                         class="w-full map"
                         style="height: 60rem"
-                      >
-                        <div id="popup"></div>
-                      </div>
+                      ></div>
                     </div>
                   </section>
                 </div>
@@ -359,14 +357,13 @@
 
 <script>
 import { mapGetters, mapState } from 'vuex'
-// import { gmapApi } from 'gmap-vue'
 import { mixin as clickaway } from 'vue-clickaway'
 // import VueSimpleRangeSlider from 'vue-simple-range-slider'
 // import 'vue-simple-range-slider/dist/vueSimpleRangeSlider.css'
 
 import 'ol/ol.css'
 import { Circle, Fill, Style } from 'ol/style'
-import { Feature, Map, Overlay, View } from 'ol/index'
+import { Feature, Map, View } from 'ol/index'
 import { OSM, Vector as VectorSource } from 'ol/source'
 import { Point } from 'ol/geom'
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
@@ -393,51 +390,33 @@ export default {
     radius: null,
     markers: [
       {
-        position: { lat: 52.379189, lng: 4.899431 },
-        price: '$200',
-        name: 'Bali Property for Sale – Chill House Hipster Retreat',
-        image: 'property1.jpg',
-        location: 'Canggu, Pererenan',
+        position: new Point([5.222124, 52.171353]),
+
         clicked: false,
       },
       {
-        position: { lat: 52.345999, lng: 4.821177 },
-        price: '$1800',
-        name: 'Flawless Uluwatu Villa Zsa Zsa Finally for Sale',
-        image: 'property2.jpg',
-        location: 'Canggu, Tabanan, Tanah Lot',
+        position: new Point([5.122124, 50.371353]),
+
         clicked: false,
       },
       {
-        position: { lat: 52.345999, lng: 4.821177 },
-        price: '$1300',
-        name: 'High Ranking Boutique Resort for Sale in Sanur',
-        image: 'property3.jpg',
-        location: 'Bukit, Ungasan',
+        position: new Point([5.92124, 51.371353]),
+
         clicked: false,
       },
       {
-        position: { lat: 52.335999, lng: 4.726177 },
-        price: '$3900',
-        name: 'Modern Bali Villa for Rent in Seminyak',
-        image: 'property4.jpg',
-        location: 'Ubud, Tegallalang',
+        position: new Point([4.592124, 52.371353]),
+
         clicked: false,
       },
       {
-        position: { lat: 52.395999, lng: 4.821197 },
-        price: '$2200',
-        name: 'Chic Serenity in Beach Lovers Paradise – Sanur.',
-        image: 'property5.jpg',
-        location: 'Canggu, Berawa',
+        position: new Point([5.292124, 52.371353]),
+
         clicked: false,
       },
       {
-        position: { lat: 52.355999, lng: 4.826177 },
-        price: '$550',
-        name: 'Breathtaking Exotic Sanur Residence',
-        image: 'property6.jpg',
-        location: 'Canggu, Mengwi, Tumbak',
+        position: new Point([5.522124, 52.371353]),
+
         clicked: false,
       },
     ],
@@ -495,12 +474,8 @@ export default {
     currentZip() {
       return this.postcode
     },
-    // google: gmapApi,
   },
   async mounted() {
-    this.current_position.lat = this.center.lat
-    this.current_position.lng = this.center.lng
-    this.radius = 30 * 1000 // 30 km
     this.$axios
       .get('/messages')
       .then((response) => (this.allMessages = response))
@@ -516,7 +491,7 @@ export default {
     initiateMap() {
       const place = [5.222124, 52.371353]
       const point = new Point(place)
-      const placeTwo = [5.322124, 52.371353]
+      const placeTwo = [0.322124, 52.371353]
       const pointTwo = new Point(placeTwo)
 
       const map = new Map({
@@ -536,144 +511,31 @@ export default {
             style: new Style({
               image: new Circle({
                 radius: 9,
-                fill: new Fill({ color: 'green' }),
+                fill: new Fill({ color: '#005AA3' }),
               }),
             }),
           }),
         ],
       })
+
       console.log(map, 'map')
-      const element = document.getElementById('popup')
-      const popup = new Overlay({
-        element,
-        positioning: 'bottom-center',
-        stopEvent: false,
-        offset: [0, 0],
+
+      // map over markers to set marker in openlayer
+      this.markers.forEach((marker) => {
+        const markerLayer = new VectorLayer({
+          source: new VectorSource({
+            features: [new Feature(marker.position)],
+          }),
+          style: new Style({
+            image: new Circle({
+              radius: 9,
+              fill: new Fill({ color: '#005AA3' }),
+            }),
+          }),
+        })
+        map.addLayer(markerLayer)
+        console.log(markerLayer, 'markerLayer')
       })
-      map.addOverlay(popup)
-    },
-
-    rad(x) {
-      return (x * Math.PI) / 180
-    },
-    getDistance(p1, p2) {
-      /* Haversine formula */
-      const R = 6378137 // Earth’s mean radius in meter
-      const dLat = this.rad(p2.lat() - p1.lat())
-      const dLong = this.rad(p2.lng() - p1.lng())
-      const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(this.rad(p1.lat())) *
-          Math.cos(this.rad(p2.lat())) *
-          Math.sin(dLong / 2) *
-          Math.sin(dLong / 2)
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-      const d = R * c
-      return d // returns the distance in meter
-    },
-    getDistanceTo() {
-      // eslint-disable-next-line camelcase
-      const current_cursor = new this.google.maps.LatLng(
-        this.current_position.lat,
-        this.current_position.lng
-      )
-      // object map
-
-      const restaurant = new this.google.maps.LatLng(
-        this.current_distance.restaurant.lat,
-        this.current_distance.restaurant.lng
-      )
-      const atm = new this.google.maps.LatLng(
-        this.current_distance.atm.lat,
-        this.current_distance.atm.lng
-      )
-      const cafe = new this.google.maps.LatLng(
-        this.current_distance.cafe.lat,
-        this.current_distance.cafe.lng
-      )
-      const pharmacy = new this.google.maps.LatLng(
-        this.current_distance.pharmacy.lat,
-        this.current_distance.pharmacy.lng
-      )
-      // eslint-disable-next-line camelcase
-      const convenience_store = new this.google.maps.LatLng(
-        this.current_distance.convenience_store.lat,
-        this.current_distance.convenience_store.lng
-      )
-
-      this.distance_from.restaurant = (
-        this.getDistance(current_cursor, restaurant) / 1000
-      ).toFixed(2)
-      this.distance_from.atm = (
-        this.getDistance(current_cursor, atm) / 1000
-      ).toFixed(2)
-      this.distance_from.cafe = (
-        this.getDistance(current_cursor, cafe) / 1000
-      ).toFixed(2)
-      this.distance_from.pharmacy = (
-        this.getDistance(current_cursor, pharmacy) / 1000
-      ).toFixed(2)
-      this.distance_from.convenience_store = (
-        this.getDistance(current_cursor, convenience_store) / 1000
-      ).toFixed(2)
-    },
-    updateZoom(e) {
-      this.current_zoom = e
-    },
-    updateCenter(e) {
-      this.current_position.lat = e.lat()
-      this.current_position.lng = e.lng()
-    },
-    updateData() {
-      this.getDistanceTo()
-
-      this.searchLoading = true
-      setTimeout(() => {
-        this.searchLoading = false
-
-        if (this.current_zoom) {
-          if (this.current_zoom <= 7) {
-            this.circle_markers = []
-            return false
-          }
-          if (this.current_zoom === 8) this.radius = 60 * 1000 // 60 km
-          if (this.current_zoom === 9) this.radius = 50 * 1000 // 50 km
-          if (this.current_zoom === 10) this.radius = 30 * 1000 // 30 km
-          if (this.current_zoom === 11) this.radius = 20 * 1000 // 20 km
-          if (this.current_zoom >= 12) this.radius = 10 * 1000 // 10 km
-          if (this.current_zoom >= 13)
-            this.radius = (30 * 1000) / this.current_zoom
-        }
-
-        // default radius in meters
-        const searchArea = new this.google.maps.Circle({
-          center: new this.google.maps.LatLng(
-            this.current_position.lat,
-            this.current_position.lng
-          ),
-          radius: this.radius,
-        })
-
-        this.circle_markers = []
-
-        this.markers.map((x) => {
-          // eslint-disable-next-line camelcase
-          const marker_position = new this.google.maps.LatLng(
-            x.position.lat,
-            x.position.lng
-          )
-          if (
-            this.google.maps.geometry.spherical.computeDistanceBetween(
-              marker_position,
-              searchArea.getCenter()
-            ) <= searchArea.getRadius()
-          ) {
-            this.circle_markers.push(x)
-          }
-        })
-
-        this.afterLoading = true
-      }, 1000)
     },
 
     toggleInfoWindow(marker, idx) {
@@ -689,48 +551,6 @@ export default {
       this.currentMidx = idx
     },
 
-    markerInfoWindow(marker, idx) {
-      const content = `
-      <img src="/properties/${marker.image}" class="img-marker">
-      <div class="info">
-        <div class="location text-truncate">
-        <i class="mr-1 fal fa-map-marker-alt"></i> <span class="text-secondary">${marker.location}</span>
-        </div>
-        <div class="mt-2 title text-truncate">
-          ${marker.name}
-        </div>
-        <div class="mt-1 mb-2 price">
-          ${marker.price}
-        </div>
-        <span class="pl-0 mr-1 font-weight-normal bd-right badge">
-          <i class="mr-2 far fa-bed fa-lg"></i><span style="font-size:14px;">2</span>
-        </span>
-        <span class="pl-0 mr-1 font-weight-normal bd-right badge">
-          <i class="mr-2 far fa-bath fa-lg"></i><span style="font-size:14px;">2</span>
-        </span>
-        <span class="pl-0 mr-1 font-weight-normal bd-right badge">
-          <i class="mr-2 far fa-expand-arrows fa-lg"></i><span style="font-size:14px;">2 are</span>
-        </span>
-        <span class="pl-0 mr-1 font-weight-normal bd-right badge">
-          <i class="mr-2 far fa-home fa-lg"></i><span style="font-size:14px;">1300 m²</span>
-        </span>
-      </div>`
-
-      this.infoWindowPos = marker.position
-      this.infoOptions.content = content
-
-      // check if its the same marker that was selected if yes toggle
-      if (this.currentMidx === idx) {
-        this.infoWinOpen = !this.infoWinOpen
-      }
-      // if different marker set infowindow to open and reset current marker index
-      else {
-        marker.clicked = true
-        this.infoWinOpen = true
-        this.currentMidx = idx
-      }
-    },
-
     openRangeSlider() {
       this.isRangeOpen = !this.isRangeOpen
     },
@@ -741,97 +561,3 @@ export default {
   layout: 'app',
 }
 </script>
-
-<style>
-.gm-ui-hover-effect {
-  display: none !important;
-}
-.gm-style-iw-d {
-  overflow: hidden !important;
-}
-.gm-style .gm-style-iw-t::after {
-  top: 38px;
-}
-.gm-control-active.gm-fullscreen-control {
-  border-radius: 8px !important;
-}
-
-.gm-style .gm-style-iw-c {
-  padding: 0px;
-  top: 40px;
-  border-radius: 12px;
-}
-.map-search-title {
-  font-weight: 600 !important;
-  color: rgb(34, 34, 34) !important;
-}
-.btn-red-hot {
-  background-color: #ff385c;
-  color: #fff;
-}
-.btn-red-hot:hover {
-  background-color: #ff385c;
-  color: #fff;
-}
-.info {
-  padding: 10px;
-  text-align: left;
-  overflow-wrap: break-word;
-}
-.info .location {
-  font-size: 14px !important;
-  max-width: 230px;
-}
-.info .title {
-  font-weight: 400 !important;
-  color: rgb(34, 34, 34) !important;
-  font-size: 16px !important;
-  padding-bottom: 5px;
-  max-width: 230px;
-}
-.info .price {
-  color: rgb(34, 34, 34) !important;
-  font-weight: 800;
-  font-size: 16px !important;
-}
-.img-marker {
-  object-fit: cover;
-  width: 250px;
-  height: 200px;
-}
-.text-searching {
-  z-index: 10;
-  margin: 0 auto;
-  top: 2rem;
-  font-size: 1rem;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 15px 20px;
-  align-items: center !important;
-  justify-content: center !important;
-  background: rgb(255, 255, 255) !important;
-  border-radius: 8px !important;
-}
-.gmnoprint > div {
-  border-radius: 8px !important;
-}
-.map-sticky {
-  position: sticky;
-  top: 0px;
-}
-.main {
-  overflow-y: scroll;
-  overflow-x: hidden;
-  height: 100vh;
-}
-.scroll-none {
-  width: 100vw;
-}
-.main::-webkit-scrollbar {
-  display: none;
-}
-
-.fs-14 {
-  font-size: 14px;
-}
-</style>
